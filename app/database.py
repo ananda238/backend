91389385eb92609acef6baae25a3a9475243bd40
ananda238 <syaianarjuna@gmail.com>
2026-07-22 22:58:32 +0700

    Update database.py

--- app/database.py
+++ app/database.py
@@ -6,9 +6,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# Ambil DATABASE_URL dari environment variable
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+# Validasi agar aplikasi memberikan pesan error yang jelas jika URL database kosong
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL belum disetel di environment variable!")
+
+# Buat engine database
 engine = create_engine(DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
